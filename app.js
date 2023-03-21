9224a0ea80d17d4f0b2608ea386ed1fdb0650d7a
kana3choco <110571085+kana3choco@users.noreply.github.com>
2023-03-21 12:40:33 -0500

    fixed app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-console.log("Let's get this party started!");
+// console.log("Let's get this party started!");
 
-
+const input = document.querySelector("#search");
 
 async function addGIF(input){
     const url = `http://api.giphy.com/v1/gifs/search?q=${input}&api_key=voazjthSPHXAiPG267faXBlcbJD0IgAv`;
@@ -9,37 +9,33 @@ async function addGIF(input){
     // creat div to contain GIFs
     const newGIF = document.createElement("div");
     newGIF.classList.add("newGIF");
-    if (res.data.length !== 0){
+    
+    if (res.data.data.length !== 0){
         //pick random img from the list
         const img = document.createElement("img");
-        let randomNum = Math.floor(Math.random() * res.data.length);
-        console.log(res.data[randomNum].images.original.url)
+        let randomNum = Math.floor(Math.random() * res.data.data.length);
 
-        img.src = res.data[randomNum].images.original.url;
+        img.src = res.data.data[randomNum].images.original.url;
         //append the img to GIFs
-        $("#container").append(newGIF).append(img);
+        document.querySelector("#container").appendChild(newGIF).appendChild(img);
     }else {
         alert("Try again! There are no results for the search.");
       }
-    
+  }   
+//   // add an event listener to the button so that when clicked on, the input value is selected and used as the parameters for the getGiphy function
 
-  
-  // select the submit button element
-  const btn = document.querySelector("#getGifsBtn");
-  
-  // add an event listener to the button so that when clicked on, the input value is selected and used as the parameters for the getGiphy function
-  btn.addEventListener("click", function (e) {
+const btn = document.querySelector("#getGifsBtn");
+btn.addEventListener('click', function (e) {
+    console.log(e);
     e.preventDefault();
-    const input = document.querySelector("#search");
     addGIF(input.value);
-    // resets the input box to empty
+    // // resets the input box to empty
     input.value = "";
   });
   
-  const removeBtn = document.querySelector("#removeBtn");
+//   const removeBtn = document.querySelector("#removeBtn");
   
-  // when the 'removeBtn' is clicked on, the container is emptied of all the images and the remove button
+//   // when the 'removeBtn' is clicked on, the container is emptied of all the images and the remove button
   $("#removeBtn").on("click", function (event) {
     $("#container").empty();
  })
-}
